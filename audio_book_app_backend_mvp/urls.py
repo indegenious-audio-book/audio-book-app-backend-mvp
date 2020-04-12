de53f6cae665d1cdbb2a0d348ccc68b7bf23d1bf
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2020-04-12 15:30:09 +0530

    mvp backend starting | adding urls.

--- audio_book_app_backend_mvp/urls.py
+++ audio_book_app_backend_mvp/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from audio_book_app_backend_mvp.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'books', views.BooksViewSet)
 
 urlpatterns = [
+    path('',  include(router.urls)),
     path('admin/', admin.site.urls),
 ]
