3850e662b6c6dc1493621c36fe0c0f1bee002bb5
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2020-04-12 15:16:52 +0530

    mvp backend starting | adding views and serializers.

---
+++ audio_book_app_backend_mvp/quickstart/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from . import models
+
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Books
+        fields = ('book_id', 'book_title', 'author_name', 'published_year')
+
+
+class ChaptersSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Chapters
+        fields = ('chapter_id', 'chapter_title', 'book')
--- audio_book_app_backend_mvp/quickstart/views.py
+++ audio_book_app_backend_mvp/quickstart/views.py
@@ -1,3 +1,14 @@
+from rest_framework import viewsets
+from rest_framework import permissions
 from django.shortcuts import render
+from . import models
+from . import serializers
 
 # Create your views here.
+class BooksViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that lets users view all the books
+    """
+    queryset = models.Books.objects.all()
+    serializer_class = serializers.BookSerializer
+    #permission_classes = [permissions.IsAuthenticated]
