2cc420af2894d64c10f2476844d64ad8e1366ee4
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2020-04-12 14:54:39 +0530

    mvp backend starting | create models for books and chapters.

--- audio_book_app_backend_mvp/quickstart/models.py
+++ audio_book_app_backend_mvp/quickstart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    book_id = models.IntegerField(primary_key=True)
+    book_title = models.CharField(max_length=100, primary_key=True)
+    author_name = models.CharField(max_length=100)
+    published_year = models.IntegerField()
+
+
+class Chapters(models.Model):
+    chapter_id = models.IntegerField(primary_key=True)
+    chapter_title = models.CharField(max_length=100, primary_key=True)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+
+
+
